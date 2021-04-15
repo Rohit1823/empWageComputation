f3b2f00e1c39c67dd580f4fed00c6ea23938613c
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 19:57:31 +0530

    ADDED : UC-4 Solving Using Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,31 +1,20 @@
 #!/bin/bash -x
 
 
-isPresent=1;
-randomCheck=$(( RANDOM % 2 ));
 
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-randomCheck=$(( RANDOM % 3 ));
+empCheck=$(( RANDOM % 3 ));
 
-if [ $isPresent -eq $randomCheck ];
-if [$isFullTime -eq $randomCheck ];
-then
-	empRatePerHr=20;
-        empHrs=8;
-        salary=$(( $empHrs * $empRatePerHr ));
-        echo "Salary of an Employee is $salary"        
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-	empHrs=4;
-else
-	salary=0;
-        echo "Employee is Absent";
-	empHrs=0;
-fi
+case $empCheck in
+	$isFullTime)
+		empHrs=8;;
+	$isPartTime)
+		empHrs=4;;
+	*)
+		empHrs=0;;
+esac
 
 salary=$(( $empHrs * $empRatePerHr ));
 
-echo "Salary of an Employee is $salary"
