e0c839f757f9fa3da3a8bbe60e34245ab348bb21
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 20:25:01 +0530

    ADDED : UC-6 calculate Wages of Total Hrs or Days for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,28 +1,35 @@
-#!/bin/bash -x
+# /bin/bash -x
 
 
+# CONSTANTS FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
-numWorkingDays=20;
+# VARIABLES
+totalEmpHr=0;
+totalWorkingDays=0;
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+	(( totalWorkingDays++ ))
+
      	empCheck=$(( RANDOM % 3 ));
 
 	case $empCheck in
-	$isFullTime)
+	$IS_FULL_TIME)
 		empHrs=8;;
-	$isPartTime)
+	$IS_PART_TIME)
 		empHrs=4;;
 	*)
 		empHrs=0;;
 	esac
 
-	salary=$(( $empHrs * $empRatePerHr ));
-	totalSalary=$(( $totalSalary + $salary ));
+	totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
 
 done
+
+totalSalary=$(( $totalEmpHrs * $EMP_RATE_PER_HR ));
 
