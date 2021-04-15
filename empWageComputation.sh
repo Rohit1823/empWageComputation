39826c70cc47fcd569cd4c649252026d0e5327aa
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 20:39:57 +0530

    REFACTOR : Used Function to get Work Hrs

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,27 +9,32 @@ EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
 # VARIABLES
-totalEmpHr=0;
+totalWorkHours=0;
 totalWorkingDays=0;
 
-while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
-do
-	(( totalWorkingDays++ ))
-
-     	empCheck=$(( RANDOM % 3 ));
-
-	case $empCheck in
+function getWorkingHours() {
+case $1 in
 	$IS_FULL_TIME)
-		empHrs=8;;
+		empHrs=8
+	        ;;
 	$IS_PART_TIME)
-		empHrs=4;;
+		empHrs=4
+                ;;
 	*)
 		empHrs=0;;
 	esac
 
-	totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
+	echo $workHours
+}
+
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+	(( totalWorkingDays++ ))
+	workHours="$( getWorkingHours $(( RANDOM % 3 )))"
+	totalWorkHours=$(( $totalWorkHours + $workHours ))
+
 
 done
 
-totalSalary=$(( $totalEmpHrs * $EMP_RATE_PER_HR ));
+totalSalary=$(( $totalWorkHrs * $EMP_RATE_PER_HR ));
 
