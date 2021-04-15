98e4bac3bc011a1ecb9bbfd8be090cde0287cdea
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 19:34:24 +0530

    ADDED : UC-2 Calculating Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,8 +6,12 @@ randomCheck=$(( RANDOM % 2 ));
 
 if [ $isPresent -eq $randomCheck ];
 then
-        echo "Employee is Present";
+	empRatePerHr=20;
+        empHrs=8;
+        salary=$(( $empHrs * $empRatePerHr ));
+        echo "Salary of an Employee is $salary"        
 else
+	salary=0;
         echo "Employee is Absent";
 fi
 
