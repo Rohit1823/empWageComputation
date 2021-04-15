bf42bb50e9f2e322b7617f9ac17b134c63ad3c2d
Rohit1823 <rohitmachale1823@gmail.com>
2021-04-15 20:48:50 +0530

    ADDED : UC-8 Store Daily Wage with Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -27,14 +27,21 @@ case $1 in
 	echo $workHours
 }
 
+function calcDailyWage() {
+	local workHrs=$1
+	wage=$(( $workHrs * $EMP_RATE_PER_HR ))
+	echo $wage
+}
+
 while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	(( totalWorkingDays++ ))
 	workHours="$( getWorkingHours $(( RANDOM % 3 )))"
 	totalWorkHours=$(( $totalWorkHours + $workHours ))
-
+	empDailyWage[ $totalWorkingDays ]="$( calcDailyWage $workHours )"
 
 done
 
-totalSalary=$(( $totalWorkHrs * $EMP_RATE_PER_HR ));
+totalSalary="$( calcDailyWage $totalWorkHours )"
+echo "Daily Wage " ${empDailyWage[@]}
 
